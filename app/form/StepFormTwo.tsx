d4b4f7179d36ec5bad252d057f61c32bde754f0a
somidhroy <somidhroy@gmail.com>
2023-02-04 02:01:03 +0530

    forms and zustand added

--- app/form/StepFormTwo.tsx
+++ app/form/StepFormTwo.tsx
@@ -2,16 +2,10 @@ import React from 'react'
 
 const StepForm2 = () => {
   return (
-    <div>
-       <form className='grid grid-cols-3 gap-10'>
-        <input type="text" className="border-2" placeholder="Form 2" />
-        <input type="text" className="border-2" placeholder="Form 2" />
-        <input type="text" className="border-2" placeholder="Form 2" />
-        <input type="text" className="border-2" placeholder="Form 2" />
-        <input type="text" className="border-2" placeholder="Form 2" />
-        <input type="text" className="border-2" placeholder="Form 2" />
-        <input type="text" className="border-2" placeholder="Form 2" />
-      </form>
+    <div className='flex items-center justify-center gap-5'>
+      <span className='border-2 p-2 border-black'>Mass gain</span>
+      <span className='border-2 p-2 border-black'>Stay fit</span>
+      <span className='border-2 p-2 border-black'>Weight Loss</span>
     </div>
   )
 }
