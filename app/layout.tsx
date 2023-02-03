06ed76d7c12bfde6a24913f5e501c6a4cdfdfe5f
Nishantjain10 <nishant.jain.775@gmail.com>
2023-02-04 01:07:34 +0530

    Updated Navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import './globals.css'
+import Navbar from "./components/Navbar";
 
 export default function RootLayout({
   children,
@@ -7,12 +8,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
-      <head />
-      <body>{children}</body>
-    </html>
+    <body>
+      <Navbar />
+      {children}
+    </body>
+  </html>
   )
 }
