d1ec759e565a6e13ab3fe6f3d629e781a3b14ecd
somidhroy <somidhroy@gmail.com>
2023-02-04 23:07:49 +0530

    form two updated

--- app/form/Container.tsx
+++ app/form/Container.tsx
@@ -7,8 +7,8 @@ import StepFormThree from "./StepFormThree";
 import StepFormFour from "./StepFormFour";
 import { buildStyles, CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
-import { Button } from "@mui/material";
-import Link from "next/link";
+import { useRouter } from "next/navigation";
+
 
 
 const Container = () => {
@@ -19,6 +19,7 @@ const Container = () => {
     <StepFormFour />,
   ];
   const [formIndex, setFormIndex] = useState(0);
+  const route = useRouter()
 
   const handleRightClick = () => {
     setFormIndex((prev) => (prev < 3 ? prev + 1 : prev));
@@ -28,33 +29,50 @@ const Container = () => {
     setFormIndex((prev) => (prev > 0 ? prev - 1 : prev));
   };
 
+  const changePercent = () => {
+    percentage = 100
+  }
+
   let percentage = (formIndex) / 4 * 100
-  console.log(formIndex);
+  const handleClick = () => {
+
+    changePercent()
+     setTimeout(() => {
+      route.push('/')
+    }, 3000)
+
+  }
+
+  console.log(percentage)
+
 
   return (
-    <div className="flex mx-auto my-[10em] w-[80em]  h-[30em] overflow-hidden">
+    <div className="flex mx-auto my-[10em] w-[100em]  h-[40em] overflow-hidden rounded-xl">
       <div className="my-0  h-full relative  flex-1 border-2   p-2">
         {forms[formIndex]}
 
 
         {formIndex === 3 ?
-          <Link
-            className="absolute right-0 bottom-0 cursor-pointer"
-            href={'/dashboard'}>Finish</Link>
+          <button
+            onClick={handleClick}
+            className="absolute right-[90px] bottom-10 cursor-pointer bg-black px-4 py-2 text-white"
+          >Finish</button>
           :
           <button
-            className="absolute right-0 bottom-0 cursor-pointer"
+            className="absolute right-[90px] bottom-10 cursor-pointer bg-black px-4 py-2 text-white"
             onClick={handleRightClick
             }>Next</button>
         }
 
         <button
-          className="absolute left-0  bottom-0 cursor-pointer"
+          className="absolute left-[90px] bottom-10 cursor-pointer bg-black px-4 py-2 text-white"
           onClick={handleLeftClick}>Prev</button>
 
       </div>
 
-      <div className=" bg-[#eab308] text-white flex-col flex-1 flex items-center justify-center  font-bold text-5xl">
+      <div className=" bg-[#eab308] text-white flex-col w-[10em] flex items-center justify-center  font-bold text-5xl ">
+
+        <h1>Progress</h1>
         <div style={{ width: 200, height: 200 }}>
           <CircularProgressbar
             value={percentage}
@@ -64,6 +82,8 @@ const Container = () => {
             })}
           />
         </div>
+
+        <h1 className="text-xl mt-20 ">Here will be progress bar</h1>
       </div>
 
 
