0e222e638438f802909a1e6696c3caba88700666
somidhroy <somidhroy@gmail.com>
2023-02-04 10:45:56 +0530

    adding third form

--- app/form/StepFormFour.tsx
+++ app/form/StepFormFour.tsx
@@ -1,17 +1,102 @@
 import React from 'react'
+import useFormOneStore from '@/store/formStore'
+import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/ButtonGroup';
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
+import InputAdornment from '@mui/material/InputAdornment';
+import Input from '@mui/material/Input';
+import { MenuItem, Select, SelectChangeEvent } from '@mui/material';
 
 const StepForm4 = () => {
+
+  const [selectedPlan, timeDuration, setTimeDuration] = useFormOneStore(state => [
+    state.selectedPlan, state.timeDuration, state.setTimeDuration])
+
+  const handleChange = (e: SelectChangeEvent) => {
+    setTimeDuration(e.target.value)
+  }
+
   return (
     <div>
-       <form className='grid grid-cols-3 gap-10'>
-        <input type="text" className="border-2" placeholder="Form 4" />
-        <input type="text" className="border-2" placeholder="Form 4" />
-        <input type="text" className="border-2" placeholder="Form 4" />
-        <input type="text" className="border-2" placeholder="Form 4" />
-        <input type="text" className="border-2" placeholder="Form 4" />
-        <input type="text" className="border-2" placeholder="Form 4" />
-        <input type="text" className="border-2" placeholder="Form 4" />
-      </form>
+
+      {selectedPlan === 'exercise' ? (
+        <div>
+          <div>
+            <h1 className='font-bold text-3xl'>Type of exercise</h1>
+            <ButtonGroup size="large" aria-label="large button group">
+              <Button>Home workout</Button>
+              <Button>Calesthenics</Button>
+              <Button>Weight lifting</Button>
+              <Button>Yoga</Button>
+            </ButtonGroup>
+          </div>
+          <div>
+            <h1 className='text-3xl font-bold'>Experience</h1>
+            <ButtonGroup size='large' aria-label="large button group">
+              <Button>Beginner</Button>
+              <Button>Intermediate</Button>
+              <Button>Expert</Button>
+            </ButtonGroup>
+          </div>
+        </div>
+      ) : (
+        <div>
+          <div>
+
+            <h1 className='text-3xl font-bold'>What do you prefer in eating?</h1>
+            <ButtonGroup size='large' aria-label="large button group">
+              <Button>Veg</Button>
+              <Button>Non-veg</Button>
+              <Button>Both</Button>
+              <Button>Vegan</Button>
+            </ButtonGroup>
+          </div>
+
+          <div>
+            <h1 className='text-3xl font-bold'>What's your budget?</h1>
+            <FormControl fullWidth sx={{ m: 1 }} variant="standard">
+              <InputLabel htmlFor="standard-adornment-amount">Amount</InputLabel>
+              <Input
+                className='w-[15em]'
+                id="standard-adornment-amount"
+                startAdornment={<InputAdornment position="start">₹</InputAdornment>}
+              />
+            </FormControl>
+          </div>
+
+          <div>
+            <h1 className='text-3xl font-bold'>Type of Diet</h1>
+            <ButtonGroup size='large' aria-label="large button group">
+              <Button>Home Diet</Button>
+              <Button>Hostel Diet</Button>
+
+            </ButtonGroup>
+          </div>
+
+
+        </div>
+      )}
+
+      <div className='mt-10'>
+        <FormControl fullWidth>
+          <InputLabel id="demo-simple-select-label">Time Duration</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={timeDuration}
+            label="Time Duration"
+            onChange={handleChange}
+            defaultValue={'Three Month'}
+          >
+            <MenuItem value={"One Month"}>One Months</MenuItem>
+            <MenuItem value={"Three Month"}>Three Months</MenuItem>
+            <MenuItem value={"Six Month"} >Six Months</MenuItem>
+          </Select>
+        </FormControl>
+      </div>
+
+
     </div>
   )
 }
--- store/formStore.ts
+++ store/formStore.ts
@@ -1,38 +1,67 @@
 import { create } from "zustand";
 
 type State = {
-  age: string;
-  height: String;
-  weight: string;
-  diseases: string;
-  allergies: string;
-  seletedType: string
+    age: string;
+    height: String;
+    weight: string;
+    diseases: string;
+    allergies: string;
+    selectedType: string
+    selectedPlan: string
+    exerciseType: string
+    exerciseExperience: string
+    foodPreference: string
+    dietBudget: string
+    dietType: string
+    timeDuration: string
 };
 
 type Actions = {
-  setAge: (age: string) => void;
-  setHeight: (height: string) => void;
-  setWeight: (weight: string) => void;
-  setDisease: (diseases: string) => void;
-  setAllergies: (allergies: string) => void;
-  setSelectedType: (type: State['seletedType']) => void
+    setAge: (age: string) => void;
+    setHeight: (height: string) => void;
+    setWeight: (weight: string) => void;
+    setDisease: (diseases: string) => void;
+    setAllergies: (allergies: string) => void;
+    setSelectedType: (type: string) => void
+    setSelectedPlan: (plan: string) => void
+    setExerciseType: (type: string) => void
+    setExerciseExperience: (exp: string) => void
+    setFoodPreference: (food: string) => void
+    setDietBudget: (budget: string) => void
+    setDietType: (type: string) => void
+    setTimeDuration: (time: string) => void
 };
 
 const useFormOneStore = create<State & Actions>((set) => ({
-  age: "",
-  height: "",
-  weight: "",
-  diseases: "",
-  allergies: "",
-  seletedType: "",
-  
+    age: "",
+    height: "",
+    weight: "",
+    diseases: "",
+    allergies: "",
+    selectedType: "",
+    selectedPlan: "",
+    exerciseType: "",
+    exerciseExperience: "",
+    foodPreference: "",
+    dietBudget: "",
+    dietType: "",
+    timeDuration: "",
 
-  setAge: (age) => set(() => ({ age: age })),
-  setHeight: (height) => set(() => ({ height: height })),
-  setWeight: (weight) => set(() => ({ weight: weight })),
-  setDisease: (diseases) => set(() => ({ diseases: diseases })),
-  setAllergies: (allergies) => set(() => ({ allergies: allergies })),
-   setSelectedType: (type) => set(() => ({ seletedType: type }))
+
+
+    setAge: (age) => set(() => ({ age: age })),
+    setHeight: (height) => set(() => ({ height: height })),
+    setWeight: (weight) => set(() => ({ weight: weight })),
+    setDisease: (diseases) => set(() => ({ diseases: diseases })),
+    setAllergies: (allergies) => set(() => ({ allergies: allergies })),
+    setSelectedType: (type) => set(() => ({ selectedType: type })),
+    setSelectedPlan: (plan) => set(() => ({ selectedPlan: plan })),
+    setExerciseType: (type) => set(() => ({ exerciseType: type })),
+    setExerciseExperience: (exp) => set(() => ({ exerciseExperience: exp })),
+    setFoodPreference: (food) => set({ foodPreference: food }),
+    setDietBudget: (budget) => set({ dietBudget: budget }),
+    setDietType: (type) => set({ dietType: type }),
+    setTimeDuration: (time) => set({ timeDuration: time })
 }));
 
 export default useFormOneStore;
