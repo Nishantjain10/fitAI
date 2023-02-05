b4719e5d98cba9e407d480cb6a92b1e430a03aa5
aadarsh805 <aadarshthakur80501@gmail.com>
2023-02-05 05:59:51 +0530

    links buttons routing fix

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,52 +1,106 @@
 "use client";
 import Link from "next/link";
 import type { FC } from "react";
-import { useState } from "react";
-import fitAIimg from '@/public/images/flexed-biceps_fitAI.png'
-import Image from 'next/image'
-
+import { useState, useEffect } from "react";
+import fitAIimg from "@/public/images/flexed-biceps_fitAI.png";
+import Image from "next/image";
+import { account } from "@/pages/api/appwriteConfig";
+import { useRouter } from "next/navigation";
 
 interface NavbarProps {}
 
+interface UserData {
+  $id?: string;
+  name?: string;
+  email?: string;
+  emailVerification?: boolean;
+  charAt?: string;
+}
 const Navbar: FC<NavbarProps> = ({}) => {
-  const [activeButton, setActiveButton] = useState('');
+  const [activeButton, setActiveButton] = useState("");
+  const [userDetails, setUserDetails] = useState<UserData>({});
+  const router = useRouter();
+
+  const handleLogout = async () => {
+    try {
+      await account.deleteSession("current");
+      router.push("/");
+      window.location.reload();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    const getData = account.get();
+    getData.then(
+      function (response: any) {
+        setUserDetails(response);
+      },
+      function (error: any) {
+        console.log(error);
+      }
+    );
+  }, []);
+
+  console.log(userDetails);
 
   return (
     <div className="md:container font-product md:mx-auto py-10 px-24 flex items-center justify-between">
       <div className="text-4xl font-semibold flex flex-row justify-center items-center gap-1 text-violet-600">
-        <Image className="w-10 h-10" src={fitAIimg} alt={""}/> 
+        <Image className="w-10 h-10" src={fitAIimg} alt={""} />
         <a href="/">
-        <span> fitai. </span>
+          <span> fitai. </span>
         </a>
-        </div>
+      </div>
       <div className="flex justify-center items-center gap-10 font-medium text-violet-600 text-lg">
-
         <Link href="/">
-        <button
-        className="hover:text-violet-700"
-        id='home-button'>
-          Home
-        </button>
+          <button className="hover:text-violet-700" id="home-button">
+            Home
+          </button>
         </Link>
 
-
-        <button
-        id='trainer-button'><a href="#trainers">Trainers</a>
+        <button id="trainer-button">
+          <a href="#trainers">Trainers</a>
         </button>
 
-        
-        <Link href="/login">
-        <button
-        className="bg-violet-600 text-white rounded-md px-4 py-2 hover:bg-violet-500 transition-all"
-        id='login-button'>Login</button>
-        </Link>
+        {userDetails.email ? (
+          <div className="flex items-center gap-10">
+            <Link href="/dashboard">
+              <button
+                className=" border border-hover:border-[#F3BC34] rounded-md px-3 py-2 hover:bg-violet-600 hover:text-white transition-all"
+                id="signup-button flex items-center gap-4"
+              >
+                <span className="w-8 h-8 rounded-full bg-gray-200"></span>
+                <span>Profile</span>
+              </button>
+            </Link>
+            <button
+              className="bg-[#FF4C6E] text-white p-2 rounded-md"
+              onClick={handleLogout}
+            >
+              Logout
+            </button>
+          </div>
+        ) : (
+          <div className="flex items-center gap-12">
+            <Link href="/login">
+              <button
+                className="bg-violet-600 text-white rounded-md px-4 py-2 hover:bg-violet-500 transition-all"
+                id="login-button"
+              >
+                Login
+              </button>
+            </Link>
 
-
-        <Link href="/signup">
-        <button 
-        className=" border border-hover:border-[#F3BC34] rounded-md px-3 py-2 hover:bg-violet-600 hover:text-white transition-all"
-        id='signup-button'>Signup</button>
-        </Link>
+            <button
+              className=" border border-hover:border-[#F3BC34] rounded-md px-3 py-2 hover:bg-violet-600 hover:text-white transition-all"
+              id="signup-button"
+            >
+              Signup
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
