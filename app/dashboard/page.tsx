f1e6da2a3d99cdb3e6232d97f514573a6fa162a4
Nishantjain10 <nishant.jain.775@gmail.com>
2023-02-17 22:49:45 +0530

    fix: dashboard code prettified

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -7,8 +7,7 @@ import Link from "next/link";
 import GraphSvg from "public/images/github-contribution-grid-snake.svg";
 import Image from "next/image";
 import useFormOneStore from "@/store/formStore";
-import Loader from "../components/Loader";
-import Footer from "../components/Footer"
+import Footer from "../components/Footer";
 
 interface UserData {
   $id?: string;
@@ -49,7 +48,6 @@ const page: React.FC = () => {
   };
 
   const html = state?.answer?.replace(/\n/g, "<br>");
-
 
   return (
     <>
@@ -230,23 +228,23 @@ const page: React.FC = () => {
             />
           </div>
           <div className="font-product flex flex-row  py-12 items-center border-2 justify-center md:container md:mx-auto mt-12">
-              <div className="justify-self-center w-auto font-product font-medium">
-                <button className=" px-4 py-3 font-semibold rounded-md  bg-violet-600 text-white mx-8 mt-7 hover:scale-105 transition-all">
-                  Regenerate💫
-                </button>
-              </div>
-              <div className="justigy-self-start w-auto font-product font-medium">
-                <button className="rounded-md px-5 py-3 font-semibold  bg-violet-600 text-white mx-8 mt-7 hover:border hover:border-black  hover:scale-105 transition-all">
-                  Add your Diet🤩
-                </button>
-              </div>
-              <div className="justify-self-center w-auto font-product font-medium">
-                <button className="bg-lime-600 shadow-sm text-white px-4 py-3 font-semibold rounded-md mx-8 mt-7 hover:scale-105 transition-all">
-                  Share Profile🚀
-                </button>
-              </div>
+            <div className="justify-self-center w-auto font-product font-medium">
+              <button className=" px-4 py-3 font-semibold rounded-md  bg-violet-600 text-white mx-8 mt-7 hover:scale-105 transition-all">
+                Regenerate💫
+              </button>
             </div>
-              <Footer />
+            <div className="justigy-self-start w-auto font-product font-medium">
+              <button className="rounded-md px-5 py-3 font-semibold  bg-violet-600 text-white mx-8 mt-7 hover:border hover:border-black  hover:scale-105 transition-all">
+                Add your Diet🤩
+              </button>
+            </div>
+            <div className="justify-self-center w-auto font-product font-medium">
+              <button className="bg-lime-600 shadow-sm text-white px-4 py-3 font-semibold rounded-md mx-8 mt-7 hover:scale-105 transition-all">
+                Share Profile🚀
+              </button>
+            </div>
+          </div>
+          <Footer />
         </>
       ) : (
         <p className="mt-4">
