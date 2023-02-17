87359269c3fc4e07d2a58b0e6f06c33d3bdf2272
Nishantjain10 <nishant.jain.775@gmail.com>
2023-02-18 00:17:13 +0530

    feat: comment lines added

--- pages/api/ai.tsx
+++ pages/api/ai.tsx
@@ -10,13 +10,14 @@ const openai = new OpenAIApi(configuration);
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const completion = await openai.createCompletion({
     model: "text-davinci-003",
-    prompt: req.body.prompt,
+    prompt: req.body.prompt, // Text prompt to generate continuation from, passed in as a request body parameter
     temperature: 1,
     top_p: 1,
     frequency_penalty: 0,
     presence_penalty: 0,
-    max_tokens: 256,
+    max_tokens: 256, // Maximum number of tokens to generate in the output text
   });
 
+  // Send a response with a status code of 200 and the generated text as the response body
   res.status(200).json({ result: completion.data });
 };
--- pages/api/appwriteConfig.js
+++ pages/api/appwriteConfig.js
@@ -1,14 +1,14 @@
-import {Client, Account, Databases} from 'appwrite'
+import { Client, Account, Databases } from "appwrite";
+
 const client = new Client();
 
+// Set the endpoint URL and project ID for the client
 client
-    .setEndpoint('https://appwrite.vogueeco.us/v1')
-    .setProject('63aaeaf74d0ae48e48a5');
+  .setEndpoint("https://appwrite.vogueeco.us/v1")
+  .setProject("63aaeaf74d0ae48e48a5");
 
-export const account = new Account(client)
+// Create a new account instance using the client
+export const account = new Account(client);
 
-//Database
-
-export const databases = new Databases(client, "63aaef5036cc3edd20a3")
-
-
+// Create a new databases instance using the client and database ID
+export const databases = new Databases(client, "63aaef5036cc3edd20a3");
