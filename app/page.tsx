7ff3ffde9555d53f15417480ae361d40448eb78c
Nishantjain10 <nishant.jain.775@gmail.com>
2023-02-17 22:34:02 +0530

    feat: code prettified

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,21 @@
-
 "use client";
-import '@/app/globals.css'
+import "@/app/globals.css";
 import About from "./components/About";
 import Trainer from "./components/Trainer";
 import Hero from "./components/Hero";
 import Testimonial from "./components/Testimonial";
 import Footer from "./components/Footer";
-import Navbar from './components/Navbar';
+import Navbar from "./components/Navbar";
 
 const HomePage = () => {
   return (
     <div className="text-center">
-        <Hero />
-        <About />
-        <Trainer />
-        <Testimonial />
-        <Footer />
+      <Hero />
+      <About />
+      <Trainer />
+      <Testimonial />
+      <Footer />
     </div>
-    );
+  );
 };
 export default HomePage;
-
