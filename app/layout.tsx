3639fb86147f6e6551cc8d4f90052edc9762da2a
Nishantjain10 <nishant.jain.775@gmail.com>
2023-02-17 23:04:56 +0530

    feat: removed unwanted code and code prettified

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,10 @@
-
 "use client";
-import './globals.css'
+import "./globals.css";
 import Navbar from "./components/Navbar";
 import alanBtn from "@alan-ai/alan-sdk-web";
 import { useState, useEffect } from "react";
-import Loader from './components/Loader';
-import useFormOneStore from '@/store/formStore';
-
-
+import Loader from "./components/Loader";
+import useFormOneStore from "@/store/formStore";
 
 export default function RootLayout({
   children,
@@ -17,52 +14,21 @@ export default function RootLayout({
   const [login, setLogin] = useState(false);
   const ALAN_Key = `${process.env.NEXT_PUBLIC_ALAN_KEY!}`;
   const [state] = useFormOneStore((state) => [state]);
-  useEffect(() => {
-    // alanBtn({
-    //   key: ALAN_Key,
-    //   onCommand: (commandData) => {
-    //     //@ts-ignore
-    //     if (commandData.command === "home") {
-    //       (document.getElementById("home-button") as HTMLInputElement).click();
-    //       console.log("home-working");
-    //       console.log(document.getElementById("home-button"));
-    //     }
-    //     //@ts-ignore
-    //     if (commandData.command === "login") {
-    //       (document.getElementById("login-button") as HTMLInputElement).click();
-    //       console.log("login-working");
-    //       console.log(document.getElementById("login-button"));
-    //     }
-    //     //@ts-ignore
-    //     if (commandData.command === "signup") {
-    //       (
-    //         document.getElementById("signup-button") as HTMLInputElement
-    //       ).click();
-    //       console.log("signup-working");
-    //       console.log(document.getElementById("signup-button"));
-    //     }
-    //   },
-    // });
-  }, []);
-  if (state.loading) return (
-
-    <html lang="en">
-      <body id='root' >
-        
-      <Loader />
-
-      </body>
-    </html>
-  )
-
+  if (state.loading)
+    return (
+      <html lang="en">
+        <body id="root">
+          <Loader />
+        </body>
+      </html>
+    );
 
   return (
     <html lang="en">
-
-      <body id='root'>
+      <body id="root">
         <Navbar />
         {children}
       </body>
     </html>
-  )
+  );
 }
