38c0457160aeb475fe1aa0f71c6254bc40f52026
Nishant Jain <89480234+Nishantjain10@users.noreply.github.com>
2023-11-11 22:47:19 +0530

    fix: appwrite config updated

--- pages/api/appwriteConfig.js
+++ pages/api/appwriteConfig.js
@@ -2,13 +2,16 @@ import { Client, Account, Databases } from "appwrite";
 
 const client = new Client();
 
+const appwrite_api_key = process.env.REACT_APP_APPWRITE_API_KEY;
+const database_key = process.env.REACT_APP_DATABASE_APPWRITE_API_KEY;
+
 // Set the endpoint URL and project ID for the client
 client
   .setEndpoint("https://appwrite.vogueeco.us/v1")
-  .setProject("63aaeaf74d0ae48e48a5");
+  .setProject("appwrite_api_key");
 
 // Create a new account instance using the client
 export const account = new Account(client);
 
 // Create a new databases instance using the client and database ID
-export const databases = new Databases(client, "63aaef5036cc3edd20a3");
+export const databases = new Databases(client, database_key);
